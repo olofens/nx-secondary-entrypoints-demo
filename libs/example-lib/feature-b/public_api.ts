1f6aedb5997a841167e690e837be6120056fdf52
Olof Enström <enstrom@eliq.io>
2021-03-14 11:16:57 +0100

    add secondary entry points

--- libs/example-lib/feature-b/public_api.ts
+++ libs/example-lib/feature-b/public_api.ts
@@ -1,14 +1,2 @@
-export * from "./account.module"
+export * from "./feature-b.module"
 
-// components
-export * from "./components/account-bills/account-bills.component";
-export * from "./components/account-card/account-card.component";
-export * from "./components/account-contact/account-contact.component";
-export * from "./components/account-home/account-home.component";
-export * from "./components/account-home-property-button/account-home-property-button.component";
-export * from "./components/account-menu/account-menu.component";
-export * from "./components/account-notifications/account-notifications.component";
-export * from "./components/account-personaldetails/account-personaldetails.component";
-export * from "./components/account-tariff/account-tariff.component";
-export * from "./components/account-language/account-language.component";
-export * from "./components/account-consents/account-consents.component";
